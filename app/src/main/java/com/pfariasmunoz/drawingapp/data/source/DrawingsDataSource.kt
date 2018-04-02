9687c74a8b42ec5d3e8590f0d22b62813f87a90c
pamunoz <pfariasmunoz@gmail.com>
2018-04-02 13:49:10 -0300

    Add coroutines abstraction for the drawing data

--- app/src/main/java/com/pfariasmunoz/drawingapp/data/source/DrawingsDataSource.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/data/source/DrawingsDataSource.kt
@@ -1,2 +1,12 @@
 package com.pfariasmunoz.drawingapp.data.source
 
+import com.pfariasmunoz.drawingapp.data.Result
+import com.pfariasmunoz.drawingapp.data.source.local.Drawing
+
+interface DrawingsDataSource {
+    suspend fun getDrawings(): Result<List<Drawing>>
+    suspend fun getDrawing(drawingId: Long): Result<Drawing>
+    suspend fun saveDrawing(drawing: Drawing)
+    suspend fun deleteAllDrawings()
+    suspend fun deleteDrawing(drawingId: Long)
+}
