feaad5f0dd09bd75e05bd477e75340dc4cc46cfa
pamunoz <pfariasmunoz@gmail.com>
2018-04-02 15:41:54 -0300

    Add dependency for login

---
+++ app/src/main/java/com/pfariasmunoz/drawingapp/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.pfariasmunoz.drawingapp.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.pfariasmunoz.drawingapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
