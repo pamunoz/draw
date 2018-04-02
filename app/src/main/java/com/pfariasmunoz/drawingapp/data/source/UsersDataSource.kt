3896979ed92faeae37943f1fd7b6bacfb80b7d06
pamunoz <pfariasmunoz@gmail.com>
2018-04-02 13:55:20 -0300

    Add coroutines abstraction for the users data

--- app/src/main/java/com/pfariasmunoz/drawingapp/data/source/UsersDataSource.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/data/source/UsersDataSource.kt
@@ -1,2 +1,12 @@
 package com.pfariasmunoz.drawingapp.data.source
 
+import com.pfariasmunoz.drawingapp.data.Result
+import com.pfariasmunoz.drawingapp.data.source.local.User
+
+interface UsersDataSource {
+    suspend fun getUsers(): Result<List<User>>
+    suspend fun getUser(userId: Long): Result<User>
+    suspend fun saveUser(user: User)
+    suspend fun deleteAllUsers()
+    suspend fun deleteUser(userId: Long)
+}
