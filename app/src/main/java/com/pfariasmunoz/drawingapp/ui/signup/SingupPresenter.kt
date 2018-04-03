7fc9dd009fafeac7116b23df5631f7d885f5c137
pamunoz <pfariasmunoz@gmail.com>
2018-04-03 15:55:50 -0300

    Add save user to presenter

--- app/src/main/java/com/pfariasmunoz/drawingapp/ui/signup/SingupPresenter.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/ui/signup/SingupPresenter.kt
@@ -2,20 +2,25 @@ package com.pfariasmunoz.drawingapp.ui.signup
 
 import com.pfariasmunoz.drawingapp.data.source.UsersDataSource
 import com.pfariasmunoz.drawingapp.data.source.model.User
+import com.pfariasmunoz.drawingapp.util.launchSilent
+import kotlinx.coroutines.experimental.android.UI
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.coroutines.experimental.CoroutineContext
 
 
 class SignupPresenter(
-        val view: SignupContract.View,
-        val usersDataSource: UsersDataSource) : SignupContract.Presenter {
+        private val view: SignupContract.View,
+        private val usersDataSource: UsersDataSource,
+        private val uiContext: CoroutineContext = UI) : SignupContract.Presenter {
 
 
-    override fun saveUser() {
+    override fun saveUser() = launchSilent(uiContext) {
         val login = view.getUserName()
         val password = checkedPassword()
         val byteArray = ByteArray(10)
-        
+        usersDataSource.saveUser(User(login = login, password = password, drawing = byteArray))
+
     }
 
     override fun checkedPassword(): String {
