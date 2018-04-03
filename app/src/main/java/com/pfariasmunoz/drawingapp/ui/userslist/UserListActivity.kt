2efa633b856359ee298d6574eed826a2a26fdf70
Pablo Munoz <pfariasmunoz@gmail.com>
2018-04-02 22:52:07 -0300

    Add anko library

--- app/src/main/java/com/pfariasmunoz/drawingapp/ui/userslist/UserListActivity.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/ui/userslist/UserListActivity.kt
@@ -2,11 +2,12 @@ package com.pfariasmunoz.drawingapp.ui.userslist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.pfariasmunoz.drawingapp.R
 
 class UserListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_drawing2)
+        setContentView(R.layout.activity_user_list)
     }
 }
