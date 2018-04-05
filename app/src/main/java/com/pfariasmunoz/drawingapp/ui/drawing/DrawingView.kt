6010c811cff18fbb1f7661725ba843991e748573
pamunoz <pfariasmunoz@gmail.com>
2018-04-05 17:31:12 -0300

    Setup paint

--- app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingView.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingView.kt
@@ -1,10 +1,27 @@
 package com.pfariasmunoz.drawingapp.ui.drawing
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 
 class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+    
+    private val drawPaint: Paint
+
+    init {
+        isFocusable = true
+        isFocusableInTouchMode = true
+        drawPaint = Paint().apply {
+            color = Color.BLACK
+            isAntiAlias = true
+            strokeWidth = 5f
+            style = Paint.Style.STROKE
+            strokeJoin = Paint.Join.ROUND
+            strokeCap = Paint.Cap.ROUND
+        }
+    }
 
 }
