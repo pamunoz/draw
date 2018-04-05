7ecaad074d5d243ba05f5cdae311eaaae6046c3c
pamunoz <pfariasmunoz@gmail.com>
2018-04-05 17:37:20 -0300

    Test onDraw

--- app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingView.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingView.kt
@@ -1,6 +1,7 @@
 package com.pfariasmunoz.drawingapp.ui.drawing
 
 import android.content.Context
+import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
@@ -8,7 +9,7 @@ import android.view.View
 
 
 class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
-    
+
     private val drawPaint: Paint
 
     init {
@@ -21,6 +22,16 @@ class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
             style = Paint.Style.STROKE
             strokeJoin = Paint.Join.ROUND
             strokeCap = Paint.Cap.ROUND
+        }
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.apply {
+            drawCircle(50f, 50f, 20f, drawPaint)
+            drawPaint.color = Color.GREEN
+            drawCircle(50f, 150f, 20f, drawPaint)
+            drawPaint.color = Color.BLUE
+            drawCircle(50f, 250f, 20f, drawPaint)
         }
     }
 
