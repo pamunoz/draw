1c8476fbcdb9d5c04f4409d4868973c7926e474e
pamunoz <pfariasmunoz@gmail.com>
2018-04-06 12:40:45 -0300

    Add methods to the presenter to setup the view and find the user

--- app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingPresenter.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingPresenter.kt
@@ -1,15 +1,28 @@
 package com.pfariasmunoz.drawingapp.ui.drawing
 
+import com.pfariasmunoz.drawingapp.data.source.local.UsersLocalDataSource
+import com.pfariasmunoz.drawingapp.di.Injector
+import com.pfariasmunoz.drawingapp.util.launchSilent
 import javax.inject.Inject
+import kotlin.coroutines.experimental.CoroutineContext
 
 class DrawingPresenter @Inject constructor(): DrawingContract.Presenter {
 
-    override fun setupView(view: DrawingContract.View) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    lateinit var view: DrawingContract.View
+    val usersDataSource : UsersLocalDataSource
+    val uiContext: CoroutineContext
+
+    init {
+        this.usersDataSource = Injector.get().localUsersDataSource()
+        this.uiContext = Injector.get().coroutineUIContext()
     }
 
-    override fun findUser() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun setupView(view: DrawingContract.View) {
+        this.view = view
+    }
+
+    override fun findUser() = launchSilent(uiContext) {
+        usersDataSource.getUserById(view.currentUserId)
     }
 
 }
