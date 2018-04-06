878768a0b4abdf2a8ce64c0cd7edcbc584c86563
pamunoz <pfariasmunoz@gmail.com>
2018-04-06 12:20:38 -0300

    Add presenter to drawing ui

--- app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingContract.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingContract.kt
@@ -3,10 +3,9 @@ package com.pfariasmunoz.drawingapp.ui.drawing
 
 interface DrawingContract {
     interface View {
-
+        val currentUserId: String
     }
     interface Presenter {
-        val currentUserId: String
         fun setupView(view: View)
         fun findUser()
     }
---
+++ app/src/main/java/com/pfariasmunoz/drawingapp/ui/drawing/DrawingPresenter.kt
@@ -0,0 +1,15 @@
+package com.pfariasmunoz.drawingapp.ui.drawing
+
+import javax.inject.Inject
+
+class DrawingPresenter @Inject constructor(): DrawingContract.Presenter {
+
+    override fun setupView(view: DrawingContract.View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun findUser() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
