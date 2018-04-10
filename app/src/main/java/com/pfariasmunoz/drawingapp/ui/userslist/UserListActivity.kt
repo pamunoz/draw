0da93dba628796b055b0e540e8e6d290d890baf5
pamunoz <pfariasmunoz@gmail.com>
2018-04-10 11:07:48 -0300

    Add comments to the user list activity

--- app/src/main/java/com/pfariasmunoz/drawingapp/ui/userslist/UserListActivity.kt
+++ app/src/main/java/com/pfariasmunoz/drawingapp/ui/userslist/UserListActivity.kt
@@ -11,8 +11,13 @@ import com.pfariasmunoz.drawingapp.ui.home.HomeActivity
 import com.pfariasmunoz.drawingapp.util.launchActivity
 import kotlinx.android.synthetic.main.activity_user_list.*
 
+/**
+ * This Activity is in charge of displaying the registered users.
+ */
+@Suppress("JoinDeclarationAndAssignment")
 class UserListActivity : AppCompatActivity(), UserListContract.View {
 
+    /** The [UserListContract.Presenter] in charge of the logic */
     val presenter: UserListPresenter
 
     init {
@@ -24,7 +29,6 @@ class UserListActivity : AppCompatActivity(), UserListContract.View {
         setContentView(R.layout.activity_user_list)
         setSupportActionBar(user_list_toolbar)
         supportActionBar?.apply {
-            //setDisplayHomeAsUpEnabled(true)
             val title = findViewById<TextView>(R.id.title_toolbar)
             title?.text = resources.getString(R.string.toolbar_title_user_list)
             val button = findViewById<Button>(R.id.btn_toolbar)
@@ -36,6 +40,7 @@ class UserListActivity : AppCompatActivity(), UserListContract.View {
         setupAdapter()
     }
 
+    /** Reset the presenter and reload the user list from the database */
     override fun onResume() {
         super.onResume()
         presenter.apply {
@@ -44,6 +49,7 @@ class UserListActivity : AppCompatActivity(), UserListContract.View {
         }
     }
 
+    /** Set up a default adapter for the user list view */
     override fun setupAdapter() {
         user_list.adapter = ArrayAdapter(this, R.layout.item_user, presenter.userNamesList)
     }
